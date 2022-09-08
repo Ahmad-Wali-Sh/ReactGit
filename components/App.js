1dfef1ffb7ff66baf6e6cd0e4d9c85a89334351d
Ahmad Wali <ahmadwalisharifybtc@gmail.com>
2022-09-08 15:57:22 +0430

    this is a first commit

---
+++ components/App.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+
+export default function App () {
+    return (
+        <div className="style">
+            <h1>Hi Dear I am ReactGit.</h1>
+        </div>
+    )
+}
