ed7cb169c6bbd8c037d1b61c576914d0cd3450f7
Ahmad Wali <ahmadwalisharifybtc@gmail.com>
2022-09-08 16:03:37 +0430

    Original

--- components/App.js
+++ components/App.js
@@ -1,10 +1,12 @@
 import React from "react"
+import ComponentOne from "./ComponentOne"
 
 
 export default function App () {
     return (
         <div className="style">
             <h1>Hi Dear I am ReactGit.</h1>
+            <ComponentOne />
         </div>
     )
 }
---
+++ components/ComponentOne.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function ComponentOne () {
+    return (
+        <div className="style">
+            <h1>This is a new component pushed by git</h1>
+        </div>
+    )
+}
